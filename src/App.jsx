3087f6b0d421b65a498004d43ecbf970b75588f3
haudacia <hadassa.codes@gmail.com>
2024-04-21 23:50:04 +0200

    added first functional connection to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+//import { response } from 'express';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+//import { use } from '../../reto-5-todo-api-haudacia/src/routers/todo';
 
-import './App.css'
+const App = () => {
+  const url = 'http://localhost:3000/todo/1';
+  const [task, setTask] = useState(null);
+  useEffect(() => {
+    const getTask = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(response.status);
+        const data = await response.json();
+        setTask(data.text);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-function App() {
+    getTask();
+  }, []);
 
-  return (
-    <div>
-      YOUR APP HERE
-    </div>
-  );
-}
+  return <p>{task}</p>
+};
 
-export default App
+export default App;
