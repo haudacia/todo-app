1c3faf8b0bde1afaad07d8c83bb70a824c102879
haudacia <hadassa.codes@gmail.com>
2024-05-27 01:21:13 +0200

    debug POST method

--- src/components/handleDelete.jsx
+++ src/components/handleDelete.jsx
@@ -4,7 +4,7 @@ import { url } from "./utils";
 const handleDelete = ({ id, refreshTasks }) => {
   const confirmed = window.confirm("Are you sure you want to delete this task?");
   if (confirmed) {
-    fetch(`https://todo-app-server-cc9x.onrender.com`, {
+    fetch('https://todo-app-server-cc9x.onrender.com', {
       method: "DELETE"
     })
       .then(() => refreshTasks())
