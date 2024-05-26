218402e79963e27b7588e792bd9cbebb131bcae9
haudacia <hadassa.codes@gmail.com>
2024-05-26 18:46:36 +0200

    WIP fix url Render

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -9,7 +9,7 @@ const CreateTask = ({ refreshTasks }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const onSubmit = (formData) => {
-    fetch('https://todo-app-server-1.onrender.com', {
+    fetch('https://todo-app-3brv.onrender.com', {
       method: "POST",
       mode: 'no-cors', // Isso impede que você acesse a resposta
       credentials: 'include', // Incluir credenciais se necessário
