c278026114c7b7473ef4d230ffe09475d185e1f5
haudacia <hadassa.codes@gmail.com>
2024-05-27 02:07:00 +0200

    change property name from id to _id to match mongo's autogenerated ids

--- src/components/handleDelete.jsx
+++ src/components/handleDelete.jsx
@@ -2,6 +2,7 @@
 import { url } from "./utils";
 
 const handleDelete = ({ id, refreshTasks }) => {
+  console.log(id);
   const confirmed = window.confirm("Are you sure you want to delete this task?");
   if (confirmed) {
     fetch(`https://todo-app-server-cc9x.onrender.com/tasks/${id}`, {
